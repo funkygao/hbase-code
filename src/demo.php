9cc63dae207d26937dae94c52cd3bdb62d7349a8
funky gao <funky.gao@gmail.com>
2012-07-16 20:24:10 +0800

    format and doc enhancement

--- src/demo.php
+++ src/demo.php
@@ -3,6 +3,8 @@
  * 演示PHP通过thrift操作HBase.
  *
  * <code>
+ * /opt/app/thrift/bin/thrift -gen php /opt/app/hbase-0.90.3/src/main/resources/org/apache/hadoop/hbase/thrift/Hbase.thrift
+ *
  * cd /opt/app/hbase-0.90.3/
  * ./bin/start-hbase.sh
  * ./bin/hbase-daemon.sh start thrift
@@ -12,40 +14,56 @@
  *
  */
 
-$GLOBALS['THRIFT_ROOT'] = '/usr/lib/php';
+define('THRIFT_ROOT', '/usr/lib/php');
 
-require_once($GLOBALS['THRIFT_ROOT'] . '/Thrift.php');
-require_once($GLOBALS['THRIFT_ROOT'] . '/transport/TSocket.php');
-require_once($GLOBALS['THRIFT_ROOT'] . '/transport/TBufferedTransport.php');
-require_once($GLOBALS['THRIFT_ROOT'] . '/protocol/TBinaryProtocol.php');
+define('HBASE_HOST', '192.168.0.106');
+define('HBASE_PORT', 9090);
 
+require_once(THRIFT_ROOT . '/Thrift.php');
+require_once(THRIFT_ROOT . '/transport/TSocket.php');
+require_once(THRIFT_ROOT . '/transport/TBufferedTransport.php');
+require_once(THRIFT_ROOT . '/protocol/TBinaryProtocol.php');
 require_once dirname(__FILE__) . '/Hbase.php';
 
-//open connection
-$socket = new TSocket('192.168.0.106', 9090);
+// open connection
+$socket = new TSocket(HBASE_HOST, HBASE_PORT);
 $transport = new TBufferedTransport($socket);
 $protocol = new TBinaryProtocol($transport);
-$client = new HbaseClient($protocol);
 $transport->open();
 
-//show all tables
+// create hbase client
+$client = new HbaseClient($protocol);
+
+// show all tables
+echo "Existing tables:\n";
 $tables = $client->getTableNames();
-foreach ($tables as $name) {
+foreach ($tables as $name)
+{
     echo("  found: {$name}\n");
 }
 
-//Create a table
-try {
-    $columns = array(new ColumnDescriptor(array(
-        'name' => 'colFamily:',
-        'maxVersions' => 10)));
+// create a table with name 'tableName'
+echo "\nCreate table: tableName\n";
+try
+{
+    $columns = array(
+        new ColumnDescriptor(
+            array(
+                'name' => 'colFamily:',
+                'maxVersions' => 10,
+            )
+        )
+    );
 
     $client->createTable("tableName", $columns);
-} catch (AlreadyExists $ae) {
+}
+catch (AlreadyExists $ae)
+{
     echo("WARN: {$ae->message}\n");
 }
 
-//insert data to table
+// insert data to table
+echo "\nInsert data to table: tableName\n";
 $mutations = array(
     new Mutation(array(
         'column' => 'colFamily:Col',
@@ -54,7 +72,8 @@ $mutations = array(
 );
 $client->mutateRow("tableName", "ID_1237846634624", $mutations);
 
-//get table data
+// get table data
+echo "\nData in table: tableName\n";
 $rows = $client->getRow("tableName", "ID_1237846634624");
 foreach ($rows as $row)
 {
